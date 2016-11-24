8f11a4be9c6bc48ee4181e2eadcbec7a326862d3
Dante Fereghetti <dante.fereghetti@gmail.com>
2016-11-24 17:37:35 +0100

    Completed and updated

--- rails-mister-cocktail/config/routes.rb
+++ rails-mister-cocktail/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cocktails, only: [:show, :index, :new, :create] do
-    resources :doses, only: [:create, :destroy]
+  resources :doses, only: [:create, :destroy]
   end
   resources :ingredients, only: [:show]
 
